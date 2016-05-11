e09252bab1d2aa1fd6eb3e8030a9247a0f278f8d
Tatsuya Morita-Ahad <tatsuya.ahad@gmail.com>
2016-05-11 16:17:21 -0400

    Implemented Click and Hold System

---
+++ Assets/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+    [SerializeField]
+    private PushObject pushObjectPrefab = null;
+
+    private GameObject canvasGO = null;
+
+	// Use this for initialization
+	void Start () {
+        this.canvasGO = FindObjectOfType<Canvas>().gameObject;
+	}
+
+    private void GeneratePushCircle() {
+        PushObject obj = GameObject.Instantiate<PushObject>(this.pushObjectPrefab);
+        obj.transform.position = Input.mousePosition;
+        obj.transform.SetParent(this.canvasGO.transform);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	    if (Input.GetMouseButtonDown(0)) {
+            this.GeneratePushCircle();
+        }
+	}
+}
---
+++ Assets/PushObject.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PushObject : MonoBehaviour {
+    [SerializeField]
+    private Image refCircle = null;
+
+    [SerializeField]
+    private float targetTimeDuration = 2;
+
+    private float startTime;
+
+	// Use this for initialization
+	void Start () {
+        this.startTime = Time.time;
+
+        this.refCircle.transform.localScale = new Vector3(0, 0, 1);
+    }
+
+    private void UpdateGraphics() {
+        float deltaTime = Time.time - this.startTime;
+
+        float ratio = deltaTime / this.targetTimeDuration;
+
+        this.refCircle.transform.localScale = new Vector3(ratio, ratio, 1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.UpdateGraphics();
+
+	    if (Input.GetMouseButtonUp(0)) {
+            GameObject.Destroy(this.gameObject);
+        }
+	}
+}
