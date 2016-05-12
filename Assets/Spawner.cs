ac167285d5e64fb8394ba64214604b00ded04fa5
Tatsuya Morita-Ahad <tatsuya.ahad@gmail.com>
2016-05-12 17:00:14 -0400

    Implemented Spawner

---
+++ Assets/Spawner.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Spawner : MonoBehaviour {
+    [SerializeField]
+    private List<Pushable> spawnPrefabs;
+
+    [SerializeField]
+    private int initialSpawn = 20;
+
+    [SerializeField]
+    private Vector2 spawnDimension = new Vector2(100, 100);
+
+    [SerializeField]
+    private float spawnInterval = 2f;
+
+    private float durSinceLastSpawn = 0;
+
+    private GameObject monstersRoot = null;
+
+    private void Spawn() {
+        // TODO: Normally pick randomly, for now just first.
+        Pushable spawnPrefab = this.spawnPrefabs[0];
+
+        float xDim = this.spawnDimension.x / 2;
+        float yDim = this.spawnDimension.y / 2;
+
+        Vector3 randPos = new Vector3(Random.Range(-xDim, xDim), Random.Range(-yDim, yDim)) + this.transform.position;
+
+        Pushable spawned = GameObject.Instantiate<Pushable>(spawnPrefab);
+        spawned.transform.localPosition = randPos;
+        spawned.transform.SetParent(this.monstersRoot.transform);
+
+        this.durSinceLastSpawn = 0;
+    }
+
+	// Use this for initialization
+	void Start () {
+        this.monstersRoot = GameObject.Find("MonstersRoot");
+
+	    for (int i = 0; i < this.initialSpawn; ++i) {
+            this.Spawn();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (this.durSinceLastSpawn >= this.spawnInterval) {
+            this.Spawn();
+        }
+
+        this.durSinceLastSpawn += Time.deltaTime;
+	}
+
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(this.transform.position, this.spawnDimension);
+    }
+}
