12f059b70a8f45ab0d4d474795e3e408c2757575
Tatsuya Morita-Ahad <tatsuya.ahad@gmail.com>
2016-05-13 16:59:43 -0400

    Added net animation

--- Assets/NetObject.cs
+++ Assets/NetObject.cs
@@ -5,12 +5,29 @@ using System.Collections.Generic;
 public class NetObject : MonoBehaviour {
     List<Pushable> objToCapture = new List<Pushable>();
 
+    [SerializeField]
+    private float cooldownDuration = 1;
+
+    private Animator animator;
+
+    private float captureStartTime = 0;
+
+    private bool capturing = false;
+
     public void CaptureObjects() {
+        if (this.capturing) {
+            return;
+        }
+
         foreach(Pushable p in this.objToCapture) {
             p.Capture();
         }
 
         this.objToCapture.Clear();
+
+        this.capturing = true;
+        this.captureStartTime = Time.time;
+        this.animator.SetTrigger("Capture");
     }
     
     void OnTriggerEnter2D(Collider2D other) {
@@ -22,6 +39,17 @@ public class NetObject : MonoBehaviour {
     void OnTriggerExit2D(Collider2D other) {
         if (other.tag == "Pushable") {
             this.objToCapture.Remove(other.GetComponent<Pushable>());
+        }
+    }
+
+    void Start() {
+        this.animator = this.GetComponent<Animator>();
+    }
+
+    void Update() {
+        if (this.capturing && (Time.time - this.captureStartTime) >= this.cooldownDuration) {
+            this.animator.SetTrigger("Idle");
+            this.capturing = false;
         }
     }
 }
